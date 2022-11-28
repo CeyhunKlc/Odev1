df2bd92e084657c3035ae23a31c117de79bfeef4
CeyhunKlc <117180182+CeyhunKlc@users.noreply.github.com>
2022-11-28 09:16:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace odev1
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            int a,b, sonuc;
+                  string islem1, islem2;
+
+                  Console.Write("1.Sayıyı Giriniz:");
+
+                  islem1 = Console.ReadLine();
+                  Console.Write("2 Sayıyı Giriniz:");
+                  islem2= Console.ReadLine();
+
+                  a=Convert.ToInt32(islem1);
+                  b=Convert.ToInt32(islem2);
+
+                  sonuc = a+b;
+                  sonuc+=6;
+                
+                  Console.WriteLine(" Girmiş Olduğunuz 2 Sayının Toplamı:" + sonuc.ToString());
+
+                  Console.ReadKey();
+
+
+
+         
+
+        }
+    }
+}   
